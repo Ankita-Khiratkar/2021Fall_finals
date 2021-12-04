ee17b1f7386f5d7aca653a9e7639203be82bb7d5
Ankita-Khiratkar <aak13@illinois.edu>
2021-12-04 16:25:04 -0600

    [a] added doctests and docstrings in scraper [b] ignored folder containing files generated from scraper doctests

--- scraper.py
+++ scraper.py
@@ -1,14 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
 
-def scrape_table(i, path):
+def scrape_table(i: int, path: str):
     """
+    This function scrapes data from the below mentioned website and store them in the folder with specified file name
+    as input
+    :param i: table number specified on the website; used to scrape the particular table
+    0: job gains table; 1: job losses table
+    :param path: name of the csv file where scraped data is stored
+    :return: None
 
-    :param path:
-    :param i:
-    :return:
+    >>> scrape_table(0, './ScraperDoctestFiles/job_gains.csv')
+    >>> if 'job_gains.csv' in os.listdir('./ScraperDoctestFiles/'): print('data scraped successfully')
+    data scraped successfully
+
+    >>> scrape_table(1, './ScraperDoctestFiles/job_losses.csv')
+    >>> if 'job_losses.csv' in os.listdir('./ScraperDoctestFiles/'): print('data scraped successfully')
+    data scraped successfully
     """
     response = requests.get(
         'https://www.bls.gov/opub/ted/2021/job-gains-and-losses-by-state-from-march-2019-to-march-2021.htm')
@@ -31,11 +42,15 @@ def scrape_table(i, path):
     df.to_csv(path)
 
 
-def scrape_state_unemp(res_path):
+def scrape_state_unemp(res_path: str):
     """
-
-    :param res_path:
-    :return:
+    This function scrapes data from the below mentioned website and store them in the folder with specified file name
+    as input
+    :param res_path: name of the csv file where scraped data is stored
+    :return: None
+    >>> scrape_state_unemp('./ScraperDoctestFiles/state_unemployment_11_21.csv')
+    >>> if 'state_unemployment_11_21.csv' in os.listdir('./ScraperDoctestFiles/'): print('data scraped successfully')
+    data scraped successfully
     """
     response = requests.get(
         'https://www.bls.gov/charts/state-employment-and-unemployment/state-unemployment-rates-animated.htm')
